50ca9d348edec550a196c51b5f8a29febb930a01
Derick Hermanson <dhermanson@bushelpowered.com>
2020-04-21 05:24:10 -0500

    Add intellij stuff for awesomewm and vim keybinds

--- stows/home/awesome/.config/awesome/rc.lua
+++ stows/home/awesome/.config/awesome/rc.lua
@@ -68,7 +68,6 @@ modkey = "Mod4"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
-   awful.layout.suit.spiral.dwindle,
    awful.layout.suit.tile,
    awful.layout.suit.tile.bottom,
    awful.layout.suit.tile.left,
@@ -79,9 +78,9 @@ awful.layout.layouts = {
    awful.layout.suit.spiral.dwindle,
    awful.layout.suit.magnifier,
    awful.layout.suit.corner.nw,
-   awful.layout.suit.floating,
    awful.layout.suit.max.fullscreen,
    awful.layout.suit.max,
+   awful.layout.suit.floating,
    -- awful.layout.suit.corner.ne,
    -- awful.layout.suit.corner.sw,
    -- awful.layout.suit.corner.se,
@@ -382,8 +381,8 @@ globalkeys = gears.table.join(
    -- awful.key({ modkey, "Shift", "Control", "Mod1" }, "p", function() awful.util.spawn("mpc pause") end),
    awful.key({ modkey, "Control", "Mod1" }, "-", function() awful.util.spawn("amixer -q set Master 2dB- unmute") end),
    awful.key({ modkey, "Control", "Mod1" }, "=", function() awful.util.spawn("amixer -q set Master 2dB+ unmute") end),
-   -- awful.key({ modkey, "Control", "Mod1" }, "-", function() awful.util.spawn("amixer -q -c 1 set Speaker 2dB- unmute") end),
-   -- awful.key({ modkey, "Control", "Mod1" }, "=", function() awful.util.spawn("amixer -q -c 1 set Speaker 2dB+ unmute") end),
+   awful.key({ modkey, "Shift", "Control", "Mod1" }, "-", function() awful.util.spawn("amixer -q -c 1 set Speaker 2dB- unmute") end),
+   awful.key({ modkey, "Shift", "Control", "Mod1" }, "=", function() awful.util.spawn("amixer -q -c 1 set Speaker 2dB+ unmute") end),
    -- awful.key({ modkey, "Control", "Mod1" }, "a", function() awful.util.spawn("urxvtc -e sh -c 'alsamixer -c 1'") end),
    -- awful.key({ modkey, "Control", "Mod1" }, "l", function() awful.util.spawn("deh-lock-and-suspend") end),
    -- awful.key({ modkey, "Control", "Mod1" }, "s", function() awful.util.spawn_with_shell("sleep 0.6 && deh-scrot") end),
@@ -707,6 +706,24 @@ awful.rules.rules = {
    -- Set Firefox to always map on the tag named "2" on screen 1.
    -- { rule = { class = "Firefox" },
    --   properties = { screen = 1, tag = "2" } },
+
+   -- Intellij
+
+   {
+      rule = {
+         class = "jetbrains-.*",
+         instance = "sun-awt-X11-XWindowPeer",
+         name = "win.*"
+      },
+      properties = {
+         floating = true,
+         focus = true,
+         focusable = false,
+         ontop = true,
+         placement = awful.placement.restore,
+         buttons = {}
+      }
+   },
 }
 -- }}}
 
