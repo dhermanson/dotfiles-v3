fd819135dc10586088344472dc89f35d08380619
Derick Hermanson <dhermanson@bushelpowered.com>
2020-05-30 22:03:24 -0500

    Use solarized-dark fzf theme

--- stows/home/zsh/.zshrc.d/fzf.zsh
+++ stows/home/zsh/.zshrc.d/fzf.zsh
@@ -1,33 +1,34 @@
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
-# Base16 Zenburn
-# Author: elnawe
-
 _gen_fzf_default_opts() {
+  local base03="234"
+  local base02="235"
+  local base01="240"
+  local base00="241"
+  local base0="244"
+  local base1="245"
+  local base2="254"
+  local base3="230"
+  local yellow="136"
+  local orange="166"
+  local red="160"
+  local magenta="125"
+  local violet="61"
+  local blue="33"
+  local cyan="37"
+  local green="64"
 
-local color00='#383838'
-local color01='#404040'
-local color02='#606060'
-local color03='#6f6f6f'
-local color04='#808080'
-local color05='#dcdccc'
-local color06='#c0c0c0'
-local color07='#ffffff'
-local color08='#dca3a3'
-local color09='#dfaf8f'
-local color0A='#e0cf9f'
-local color0B='#5f7f5f'
-local color0C='#93e0e3'
-local color0D='#7cb8bb'
-local color0E='#dc8cc3'
-local color0F='#000000'
+  # Comment and uncomment below for the light theme.
 
-export FZF_DEFAULT_OPTS="
-  --color=bg+:$color01,bg:$color00,spinner:$color0C,hl:$color0D
-  --color=fg:$color04,header:$color0D,info:$color0A,pointer:$color0C
-  --color=marker:$color0C,fg+:$color06,prompt:$color0A,hl+:$color0D
-"
-
+  # Solarized Dark color scheme for fzf
+  export FZF_DEFAULT_OPTS="
+    --color fg:-1,bg:-1,hl:$blue,fg+:$base2,bg+:$base02,hl+:$blue
+    --color info:$yellow,prompt:$yellow,pointer:$base3,marker:$base3,spinner:$yellow
+  "
+  ## Solarized Light color scheme for fzf
+  #export FZF_DEFAULT_OPTS="
+  #  --color fg:-1,bg:-1,hl:$blue,fg+:$base02,bg+:$base2,hl+:$blue
+  #  --color info:$yellow,prompt:$yellow,pointer:$base03,marker:$base03,spinner:$yellow
+  #"
 }
-
-_gen_fzf_default_opts
+_gen_fzf_default_opts set background=dark
