7df4fe8f6aaa14e364a62316991f774b422a3758
Derick Hermanson <derick.hermanson@gmail.com>
2020-11-27 10:15:11 -0600

    Update RUBY_CONFIGURE_OPTS in ruby_build.zsh

--- stows/home/zsh/.zshrc.d/ruby_build.zsh
+++ stows/home/zsh/.zshrc.d/ruby_build.zsh
@@ -1,4 +1,5 @@
 # this is for homebrew's ruby-build
 # export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
-export RUBY_CONFIGURE_OPTS="--with-openssl-dir=/usr/local/opt-openssl@1.1"
+# https://github.com/rbenv/ruby-build/issues/1353#issuecomment-551348180
+export RUBY_CONFIGURE_OPTS="--with-openssl-dir=/usr/local/opt/openssl@1.1"
 
